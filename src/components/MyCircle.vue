01fe561a6b8e80f00d6ada85d71cc84132ad658e
Bebenin Grigoriy <bengor1982@yandex.ru>
2022-02-05 10:09:13 +0300

    Однокомпонент рабочий готов, фиксация перед декомпозицией

---
+++ src/components/MyCircle.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="inputCircle"
+      v-for='input in inputs'
+      :key="input.id"
+      :input = "input" 
+      >
+ 
+        <input
+          type="text" 
+          v-model="input.variableValue"
+          :min="this.minHeight"
+          :max="this.maxHeight"
+          @input="resettingTheValue($event)"
+        />
+        <input
+          class="styled-slider"
+          type="range"
+          :min="this.minHeight"
+          :max="this.maxHeight"
+          v-model="input.variableValue"
+        />
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MyCircle',
+  // props: ['changeValue'],
+  props: {changeValue: "inputs.customValue"},
+  
+  data() {
+    return {
+      minHeight: 0,
+      maxHeight: 1000,
+    }
+  },
+}
+</script>
+<style scoped>
+</style>
